e65bd74d3e791dda70a089f213110796968f4937
nlbybee <nlbybee@yahoo.com>
2024-03-28 12:35:09 -0600

    Created moving rectangles
    + Rec2d
          + creates rectangles with scale, position and color
    + Modified Image-Editor to use rectangles instead of pre-established images

--- core/src/com/mygdx/imageeditor/ImageEditor.java
+++ core/src/com/mygdx/imageeditor/ImageEditor.java
@@ -1,31 +1,50 @@
 package com.mygdx.imageeditor;
 
+import java.util.Random;
+
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class ImageEditor extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	Rec2D rectangle;
+	private Vector2 _screenSize;
+	public Random random;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		_screenSize = new Vector2(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		rectangle = new Rec2D(new Vector2(200, 100), new Vector2(200,200), new Vector2(2, 5), Color.RED);
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(0f, 0f, 0f, 1);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(rectangle.RecTexture, rectangle.Position.x, rectangle.Position.y);
+		if((rectangle.Position.x + rectangle.Scale.x) > _screenSize.x || rectangle.Position.x < 0) {
+			rectangle.Velocity.x *= -1;
+			Random random = new Random();
+			rectangle.changeColor(new Color(random.nextFloat(), random.nextFloat(), random.nextFloat(), 1));
+			}
+		if((rectangle.Position.y + rectangle.Scale.y) > _screenSize.y || rectangle.Position.y < 0) {
+			rectangle.Velocity.y *= -1;
+			Random random = new Random();
+			rectangle.changeColor(new Color(random.nextFloat(), random.nextFloat(), random.nextFloat(), 1));
+			}
+		rectangle.Position.add(rectangle.Velocity);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		rectangle.RecTexture.dispose();
 	}
 }
---
+++ core/src/com/mygdx/imageeditor/Rec2D.java
@@ -0,0 +1,40 @@
+package com.mygdx.imageeditor;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.Pixmap.Format;
+import com.badlogic.gdx.math.Vector2;
+
+public class Rec2D {
+	public Vector2 Scale;
+	public Vector2 Position;
+	public Texture RecTexture;
+	public Vector2 Velocity;
+	private Pixmap _pixelMap;
+	private Color _recColor;
+	
+	public Rec2D(Vector2 scale, Vector2 position, Vector2 velocity, Color recColor) {
+		Scale = scale;
+		Position = position;
+		_recColor = recColor;
+		Velocity = velocity;
+		generateTexture();
+	}
+	
+	private void generateTexture() {
+		_pixelMap = new Pixmap((int)Scale.x, (int)Scale.y, Format.RGBA8888);
+		_pixelMap.setColor(_recColor);
+		for(int x = 0; x < _pixelMap.getWidth(); x++) {
+			for(int y = 0; y < _pixelMap.getHeight(); y++) {
+				_pixelMap.drawPixel(x, y);
+			}
+		}
+		RecTexture = new Texture(_pixelMap);
+	}
+	
+	public void changeColor(Color newColor) {
+		_recColor = newColor;
+		generateTexture();
+	}
+}
